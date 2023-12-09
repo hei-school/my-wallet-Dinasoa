c736aee7bfd2733384ec47266690d3992234d934
dinasoa <hei.dinasoa@gmail.com>
2023-12-09 09:14:18 +0300

    feat: add .gitignore

--- driverlicense.js
+++
@@ -1,8 +0,0 @@
-import { About } from "./about.js";
-
-export class DriverLicences extends About{
-    constructor(firstname, lastname, birthdate, type){
-        super(firstname, lastname, birthdate)
-        this.type = type
-    }
-}
